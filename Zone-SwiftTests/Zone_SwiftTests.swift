ae229bc46dc28369260e0b0c54f43e681f054fbd
叶云 <550752356@qq.com>
2016-07-08 10:19:04 +0800

    Initial Commit

---
+++ Zone-SwiftTests/Zone_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Zone_SwiftTests.swift
+//  Zone-SwiftTests
+//
+//  Created by 叶云 on 16/7/8.
+//  Copyright © 2016年 叶云. All rights reserved.
+//
+
+import XCTest
+@testable import Zone_Swift
+
+class Zone_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
